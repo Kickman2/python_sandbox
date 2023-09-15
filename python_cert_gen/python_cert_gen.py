c4b303ccf2475d285bc9c59e13afa0fb857fa62c
Thet Han Kyaw <thethan_kyaw@singlife.com>
2023-09-15 15:50:05 +0800

    update

--- python_cert_gen/python_cert_gen.py
+++ python_cert_gen/python_cert_gen.py
@@ -2,13 +2,33 @@ import os
 import sys
 from OpenSSL import crypto
 
-def generate_certificate_request(common_name, key_file, csr_file, multi):
+def set_attribute(subj):
+    subj.CN = common_name
+    subj.C = "SG"
+    subj.ST = "example"
+    subj.L = "example"
+    subj.O = "example ltd"
+    subj.OU = "exampleou"
+    subj.emailAddress = "cert_admin@example.com"
+    return subj
+
+def generate_certificate_request(common_name, path, multi=False):
+    
+    key_passphrase = input("Enter a passphrase for the private key: ")
+
     key = crypto.PKey()
     key.generate_key(crypto.TYPE_RSA, 2048)
+    if not os.path.isdir(path):
+        print("{} is not found, using current dir".format(path))
+        path= "."
+
+    key_file =  path + "/" +common_name + ".key"
+    csr_file = path + "/" +common_name + ".csr"
 
     req = crypto.X509Req()
     subj = req.get_subject()
-    subj.CN = common_name
+    subj = set_attribute(subj)
+    
 
     req.set_pubkey(key)
 
@@ -21,17 +41,33 @@ def generate_certificate_request(common_name, key_file, csr_file, multi):
 
 
     with open(key_file, "wb") as keyfile:
-        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher=b'AES-256-CBC', passphrase=key_passphrase.encode()))
 
     with open(csr_file, "wb") as csrfile:
         csrfile.write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, req))
 
-def generate_self_signed_certificate(common_name, key_file, crt_file):
+def generate_self_signed_certificate(common_name, path, multi=False):
+    
+    key_passphrase = input("Enter a passphrase for the private key: ")
+
     key = crypto.PKey()
     key.generate_key(crypto.TYPE_RSA, 2048)
 
+    if not os.path.isdir(path):
+        print("{} is not found, using current dir".format(path))
+        path= "."
+    
+    key_file =  path + "/" +common_name + ".key"
+    crt_file = path + "/" +common_name + ".crt"
+
     cert = crypto.X509()
-    cert.get_subject().CN = common_name
+    subj = cert.get_subject()
+    subj = set_attribute(subj)
+ 
+    if multi:
+        san = ",".join(["DNS:" + domain.strip() for domain in input("Enter multiple domains separated by commas (e.g., domain1.com, domain2.com): ").split(",")])
+        cert.add_extensions([crypto.X509Extension(b"subjectAltName", False, san.encode())])
+
     cert.set_serial_number(1000)
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(365*24*60*60)
@@ -39,13 +75,20 @@ def generate_self_signed_certificate(common_name, key_file, crt_file):
     cert.set_pubkey(key)
     cert.sign(key, "sha256")
 
+
     with open(key_file, "wb") as keyfile:
-        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher=b'AES-256-CBC', passphrase=key_passphrase.encode()))
 
     with open(crt_file, "wb") as crtfile:
         crtfile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
 
-def generate_certificate_with_ca(common_name, key_file, crt_file, ca_key_file, ca_crt_file):
+def generate_certificate_with_ca(common_name, path , ca_key_file, ca_crt_file, multi=False):
+    
+    key_passphrase = input("Enter a passphrase for the private key: ")
+    
+    key_file =  path + "/" +common_name + ".key"
+    crt_file = path + "/" +common_name + ".crt"
+    
     ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(ca_crt_file, "rb").read())
     ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, open(ca_key_file, "rb").read())
 
@@ -53,7 +96,13 @@ def generate_certificate_with_ca(common_name, key_file, crt_file, ca_key_file, c
     key.generate_key(crypto.TYPE_RSA, 2048)
 
     cert = crypto.X509()
-    cert.get_subject().CN = common_name
+    subj = cert.get_subject()
+    subj = set_attribute(subj)
+
+    if multi:
+        san = ",".join(["DNS:" + domain.strip() for domain in input("Enter multiple domains separated by commas (e.g., domain1.com, domain2.com): ").split(",")])
+        cert.add_extensions([crypto.X509Extension(b"subjectAltName", False, san.encode())])
+
     cert.set_serial_number(1001)
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(365*24*60*60)
@@ -61,8 +110,10 @@ def generate_certificate_with_ca(common_name, key_file, crt_file, ca_key_file, c
     cert.set_pubkey(key)
     cert.sign(ca_key, "sha256")
 
+
+
     with open(key_file, "wb") as keyfile:
-        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+        keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key, cipher=b'AES-256-CBC', passphrase=key_passphrase.encode()))
 
     with open(crt_file, "wb") as crtfile:
         crtfile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
@@ -72,25 +123,21 @@ if __name__ == "__main__":
 
     if option == "1":
         common_name = input("Enter the common name for the certificate: ")
-        key_file = input("Enter the key file name (e.g., key.pem): ")
-        csr_file = input("Enter the CSR file name (e.g., csr.pem): ")
-        generate_certificate_request(common_name, key_file, csr_file)
+        path = input("Enter output path")
+        generate_certificate_request(common_name, path)
     elif option == "2":
         common_name = input("Enter the common name for the certificate: ")
-        key_file = input("Enter the key file name (e.g., key.pem): ")
-        csr_file = input("Enter the CSR file name (e.g., csr.pem): ")
-        generate_certificate_request(common_name, key_file, csr_file, multi=True)
+        path = input("Enter output path")
+        generate_certificate_request(common_name, path, multi=True)
     elif option == "3":
         common_name = input("Enter the common name for the certificate: ")
-        key_file = input("Enter the key file name (e.g., key.pem): ")
-        crt_file = input("Enter the certificate file name (e.g., cert.pem): ")
-        generate_self_signed_certificate(common_name, key_file, crt_file)
+        path = input("Enter output path")
+        generate_self_signed_certificate(common_name, path)
     elif option == "4":
         common_name = input("Enter the common name for the certificate: ")
-        key_file = input("Enter the key file name (e.g., key.pem): ")
-        crt_file = input("Enter the certificate file name (e.g., cert.pem): ")
+        path = input("Enter output path")
         ca_key_file = input("Enter the CA key file name (e.g., ca.key): ")
         ca_crt_file = input("Enter the CA certificate file name (e.g., ca.crt): ")
-        generate_certificate_with_ca(common_name, key_file, crt_file, ca_key_file, ca_crt_file)
+        generate_certificate_with_ca(common_name, path, ca_key_file, ca_crt_file)
     else:
         print("Invalid option. Please choose an option from 1 to 4.")
